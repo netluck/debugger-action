cf6d606045a44982374ce6ab2e8a3209e5a81f5e
Netluck <25601902+netluck@users.noreply.github.com>
2020-02-26 14:38:07 +0800

    Update script.sh
    30min -> 60min

--- script.sh
+++ script.sh
@@ -41,8 +41,8 @@ if [[ ! -z "$SLACK_WEBHOOK_URL" ]]; then
   curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"\`$MSG\`\"}" $SLACK_WEBHOOK_URL
 fi
 
-# Wait for connection to close or timeout in 15 min
-timeout=$((30*60))
+# Wait for connection to close or timeout in 60 min
+timeout=$((60*60))
 while [ -S /tmp/tmate.sock ]; do
   sleep 1
   timeout=$(($timeout-1))
